8a3a06c203d37078ed4218871aa569efc8ba2600
LAPTOP-N0SVLELU\VIN <Rakesh>
2024-11-20 22:37:58 +0530

    env changes initial commit

--- projects/promates/src/app/contact-us/contact-us.component.ts
+++ projects/promates/src/app/contact-us/contact-us.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NotificationService } from '../../services/notification.service';
 @Component({
   selector: 'app-contact-us',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    CommonModule,
+    FormsModule
+  ],
+  providers:[],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.scss'
 })
-export class ContactUsComponent {
+export class ContactUsComponent implements OnInit {
+  contactForm!: FormGroup;
+  constructor(private fb: FormBuilder,
+    public notificationService: NotificationService
+  ) { }
+  ngOnInit(): void {
+    this.contactForm = this.fb.group(
+      {
+        firstName: ['', Validators.required],
+        lastName: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        message: ['', Validators.required]
+      });
+  }
+  onSubmit() {
+    if (this.contactForm.valid) {
+      console.log(this.contactForm.value);
+    }else{
+      this.notificationService.notify("Please fill the mandatory fields","warning")
+    }
+  }
 
+  get firstName() { return this.contactForm.get('firstName'); } get lastName() { return this.contactForm.get('lastName'); } get email() { return this.contactForm.get('email'); } get message() { return this.contactForm.get('message'); }
 }
---
+++ projects/promates/src/services/end-points.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { environment } from '@promates.environments/environment';
+@Injectable({
+  providedIn: 'root'
+})
+export class EndPointsService {
+
+  // private API_URL_PREFIX = "/api";
+  private API_URL_PREFIX = "/workbench/api";
+  private API_VERSION_PREFIX = '/1.1';
+  private SERVER_URL: String = "";
+  public API_SERVER_URL: String;
+  private serviceList: any = {};
+  private workbenchValue = "banking";
+
+
+  constructor() {
+    if (environment.production) {
+      this.SERVER_URL = window.location.protocol + '//' + window.location.host;
+      this.API_SERVER_URL = this.SERVER_URL + this.API_URL_PREFIX;
+    } else {
+      this.API_SERVER_URL = environment['API_SERVER_URL'] + this.API_URL_PREFIX;
+    }
+    this.init();
+  }
+
+  public getServiceInfo(serviceId?: any) {
+    return this.serviceList[serviceId];
+  }
+  public init() {
+
+  }
+}
