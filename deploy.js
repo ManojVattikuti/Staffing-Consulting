cb9c50b870b7012de521b096009f7f8e0a3cb3eb
LAPTOP-N0SVLELU\VIN <Rakesh>
2024-12-22 17:03:14 +0530

    updated script

--- deploy.js
+++ deploy.js
@@ -7,6 +7,7 @@ const commitId = process.argv[2];
 const projectName = process.argv[3];
 const buildType = process.argv[4] || 'production'; // Default to production if not specified
 const buildDirectory = `dist/${projectName}/browser`; // Local build directory
+const sshKeyPath = process.env.SSH_KEY_PATH; // Access SSH key path from environment variable
 
 if (!commitId || !projectName) {
     console.error('Please provide both a commit ID and a project name.');
@@ -22,7 +23,7 @@ const deployBuild = () => {
     const serverIP = '34.162.198.231'; // Your server IP
     const serverPath = `/home/rakeshkumar_manda8161/promates/Staffing-Consulting/dist/${projectName}/browser`; // Path on server
 
-    exec(`rsync -avz ${buildDirectory}/ ${username}@${serverIP}:${serverPath}/`, (error, stdout, stderr) => {
+    exec(`rsync -avz -e "ssh -i ${sshKeyPath}" ${buildDirectory}/ ${username}@${serverIP}:${serverPath}/`, (error, stdout, stderr) => {
         if (error) {
             console.error(`Error deploying build: ${error.message}`);
             return;
@@ -36,7 +37,6 @@ const deployBuild = () => {
     });
 };
 
-// Checkout to the specified commit, install dependencies, and build the project
 // Checkout to the specified commit, install dependencies, and build the project
 git.checkout(commitId, (err) => {
     if (err) {
@@ -59,7 +59,7 @@ git.checkout(commitId, (err) => {
         console.log('Dependencies installed.');
 
         // Build the Angular project
-        exec(`ng build ${projectName} --configuration=${buildType}`, (error, stdout, stderr) => {
+        exec(`npm run build ${projectName} --configuration=${buildType}`, (error, stdout, stderr) => {
             if (error) {
                 console.error(`Error building project: ${error.message}`);
                 return;
