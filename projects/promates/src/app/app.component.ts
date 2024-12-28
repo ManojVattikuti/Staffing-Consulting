f74c6613e38d59a3f66ff75073ce79cbe8ffd0df
LAPTOP-N0SVLELU\VIN <Rakesh>
2024-12-28 21:39:22 +0530

    updated changes

--- projects/promates/src/app/app.component.ts
+++ projects/promates/src/app/app.component.ts
@@ -1,28 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet,Router,NavigationEnd  } from '@angular/router';
 import { $localize } from '@angular/localize/init';
 import { BannerComponent } from "./banner/banner.component";
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from './footer/footer.component';
 import { NotificationService } from '../services/notification.service';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
-
+import { filter } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, HeaderComponent, FooterComponent, TranslateModule],
-  providers: [],
+  providers: [Router],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
-  constructor(translate: TranslateService) {
+  constructor(translate: TranslateService,private router: Router) {
     translate.setDefaultLang('en');
     translate.use('en');
   }
 
   ngOnInit(): void {
-
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd) // Only react to NavigationEnd events
+    ).subscribe(() => {
+      window.scrollTo(0, 0); // Scroll to the top
+    });
   }
   title = $localize`:Title for the app:Welcome to our application!`;
   welcomeMessage: string = $localize`Welcome to our localized app!`;
